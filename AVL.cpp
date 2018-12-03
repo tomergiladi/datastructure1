6ff4a3b1b67e77c8ade47a4ad964b2057a0aa56e
you@example.com <you@example.com>
2018-12-03 22:32:48 +0200

    finnish insert

--- AVL.cpp
+++ AVL.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include <memory>
 using std::cout;
@@ -13,7 +14,9 @@ class AVLDict {
         T key;
         U value;
         int height;
-        explicit node(T key, U value, weak_ptr<node> parent) : parent(parent), key(key), value(value), height(0) {
+        AVLDict* tree;
+        explicit node(T key, U value, weak_ptr<node> parent, AVLDict* tree)
+            : parent(parent), key(key), value(value), height(0), tree(tree) {
         }
         ~node() {
             cout << "deleting node with value:" << this->value << endl;
@@ -24,16 +27,38 @@ class AVLDict {
                    << curr->right << ")";
             return os;
         }
+        int setHeight() {
+            int left_height = this->left->getHeight();
+            int right_height = this->right->getHeight();
+            this->height = 1 + (left_height > right_height ? left_height : right_height);
+        }
         int getHeight() {
             if (!this)
                 return -1;
             return this->height;
         }
+        int getBalanceFactor() {
+            return this->left->getHeight() - this->right->getHeight();
+        }
+        void balance() {
+            if (this->getBalanceFactor() > 1) {
+                if (this->left->getBalanceFactor() < 0) {
+                    this->left->leftRotate();
+                }
+                this->rightRotate();
+            } else if (this->getBalanceFactor() < -1) {
+                if (this->right->getBalanceFactor() > 0) {
+                    this->right->rightRotate();
+                }
+                this->leftRotate();
+            }
+        }
         void leftRotate() {
             auto right = this->right;
             weak_ptr<node> parent = this->parent;
             this->right = right->left;
-            right->left->parent = this->weak_from_this();
+            if (right->left)
+                right->left->parent = this->shared_from_this();
             right->left = this->shared_from_this();
             this->parent = right;
             right->parent = parent;
@@ -41,52 +66,119 @@ class AVLDict {
                 auto sparent = parent.lock();
                 if (this->shared_from_this() == sparent->right) {
                     sparent->right = right;
-                }
-                if (this->shared_from_this() == sparent->left) {
+                } else if (this->shared_from_this() == sparent->left) {
                     sparent->left = right;
                 }
+            } else {
+                this->tree->head = right;
             }
+            this->setHeight();
+            right->setHeight();
+        }
+        void rightRotate() {
+            auto left = this->left;
+            weak_ptr<node> parent = this->parent;
+            this->left = left->right;
+            if (left->right)
+                left->right->parent = this->weak_from_this();
+            left->right = this->shared_from_this();
+            this->parent = left;
+            left->parent = parent;
+            if (!(parent.expired())) {
+                auto sparent = parent.lock();
+                if (this->shared_from_this() == sparent->right) {
+                    sparent->right = left;
+                } else if (this->shared_from_this() == sparent->left) {
+                    sparent->left = left;
+                }
+            } else {
+                this->tree->head = left;
+            }
+            this->setHeight();
+            left->setHeight();
         }
     };
     shared_ptr<node> head;
-    static void insert(T key, U value, shared_ptr<node>& current, shared_ptr<node> parent = nullptr) {
+
+    void insert(T key, U value, shared_ptr<node>& current, shared_ptr<node> parent = nullptr) {
         if (!current) {
-            current.reset(new node(key, value, parent));
+            current.reset(new node(key, value, parent, this));
+            (this->size)++;
         } else {
             if (current->key > key)
                 insert(key, value, current->left, current);
-            if (current->key < key)
+            else if (current->key < key)
                 insert(key, value, current->right, current);
+            else {
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+                cout << "**************************************************************" << endl;
+            }
         }
-        int left_height = current->left->getHeight();
-        int right_height = current->right->getHeight();
-        current->height = 1 + (left_height > right_height ? left_height : right_height);
+        if (current->getBalanceFactor() > 1 || current->getBalanceFactor() < -1) {
+            current->balance();
+        } else {
+            current->setHeight();
+        }
     }
 
    public:
-    AVLDict() = default;
+    int size;
+    AVLDict() : size(8) {
+    }
     void insert(T key, U value) {
-        insert(key, value, this->head);
+        // insert(key, value, this->head);
+        shared_ptr<node> parent = nullptr;
+        auto* newNode = &(this->head);
+        while (*newNode) {
+            parent = *newNode;
+            if ((*newNode)->key > key)
+                newNode = &((*newNode)->left);
+            else if ((*newNode)->key < key)
+                newNode = &((*newNode)->right);
+            else {
+                newNode = nullptr;
+                break;
+            }
+        }
+        if (newNode) {
+            (*newNode).reset(new node(key, value, parent, this));
+            size++;
+            auto current = *newNode;
+            while (current) {
+                if (current->getBalanceFactor() > 1 || current->getBalanceFactor() < -1) {
+                    current->balance();
+                } else {
+                    current->setHeight();
+                }
+                current = current->parent.lock();
+            }
+        }
     }
     friend std::ostream& operator<<(std::ostream& os, const AVLDict& avl) {
         return os << avl.head << endl;
     }
-    void ttt() {
-        this->head->right->leftRotate();
-        cout << "Fdsfsa" << this->head->right->parent.lock()->key << endl;
-        cout << "Fdsfsa" << this->head->right->left->parent.lock()->key << endl;
-        cout << "Fdsfsa" << this->head->right->left->right->parent.lock()->key << endl;
+    int getHeight() {
+        return this->head->getHeight();
     }
 };
 int main() {
     AVLDict<int, int> t;
-    t.insert(1, 1);
-    t.insert(5, 5);
-    t.insert(3, 3);
-    t.insert(2, 2);
-    t.insert(10, 10);
-    t.insert(7, 7);
-    cout << t;
-    t.ttt();
-    cout << t;
+    srand(0);
+    for (int i = 0; i < 1000000; i++) {
+        int x = rand();
+        t.insert(i, i);
+        cout << t.getHeight() << "," << i << "," << t.size << endl;
+    }
 }
---
+++ longarrow.cpp
@@ -0,0 +1,9 @@
+#include "iostream"
+int main(int argc, char const* argv[]) {
+    /* code */
+    int i = 10;
+    while (i-- > 0) {
+        std::cout << i;
+    }
+    return 0;
+}
