0e66cf4dc4ad993a87a57c58038e4167c1ca7b94
you@example.com <you@example.com>
2018-12-04 18:02:55 +0200

    deleting

--- AVL.cpp
+++ AVL.cpp
@@ -53,52 +53,67 @@ class AVLDict {
                 this->leftRotate();
             }
         }
+        void removeFromParent(shared_ptr<node> replace, shared_ptr<node> parent) {
+            if (replace) {
+                replace->parent = parent;
+            }
+            if (parent) {
+                if (this->shared_from_this() == parent->right) {
+                    parent->right = replace;
+                } else if (this->shared_from_this() == parent->left) {
+                    parent->left = replace;
+                }
+            } else {
+                this->tree->head = replace;
+            }
+        }
+        void removeFromParent(shared_ptr<node> replace) {
+            this->removeFromParent(replace, this->parent.lock());
+        }
         void leftRotate() {
             auto right = this->right;
-            weak_ptr<node> parent = this->parent;
+            auto parent = this->parent.lock();
             this->right = right->left;
             if (right->left)
                 right->left->parent = this->shared_from_this();
             right->left = this->shared_from_this();
             this->parent = right;
-            right->parent = parent;
-            if (!(parent.expired())) {
-                auto sparent = parent.lock();
-                if (this->shared_from_this() == sparent->right) {
-                    sparent->right = right;
-                } else if (this->shared_from_this() == sparent->left) {
-                    sparent->left = right;
-                }
-            } else {
-                this->tree->head = right;
-            }
+            this->removeFromParent(right, parent);
             this->setHeight();
             right->setHeight();
         }
         void rightRotate() {
             auto left = this->left;
-            weak_ptr<node> parent = this->parent;
+            auto parent = this->parent.lock();
             this->left = left->right;
             if (left->right)
                 left->right->parent = this->weak_from_this();
             left->right = this->shared_from_this();
             this->parent = left;
             left->parent = parent;
-            if (!(parent.expired())) {
-                auto sparent = parent.lock();
-                if (this->shared_from_this() == sparent->right) {
-                    sparent->right = left;
-                } else if (this->shared_from_this() == sparent->left) {
-                    sparent->left = left;
-                }
-            } else {
-                this->tree->head = left;
-            }
+            this->removeFromParent(left, parent);
             this->setHeight();
             left->setHeight();
         }
     };
     shared_ptr<node> head;
+    static void printByOrder(shared_ptr<node> current) {
+        if (current) {
+            printByOrder(current->left);
+            cout << current->key << ":" << current->value << endl;
+            printByOrder(current->right);
+        }
+    }
+    static void fixFrom(shared_ptr<node> current) {
+        while (current) {
+            if (current->getBalanceFactor() > 1 || current->getBalanceFactor() < -1) {
+                current->balance();
+            } else {
+                current->setHeight();
+            }
+            current = current->parent.lock();
+        }
+    }
 
    public:
     int size;
@@ -110,11 +125,11 @@ class AVLDict {
         auto* newNode = &(this->head);
         while (*newNode) {
             parent = *newNode;
-            if ((*newNode)->key > key)
+            if ((*newNode)->key > key) {
                 newNode = &((*newNode)->left);
-            else if ((*newNode)->key < key)
+            } else if ((*newNode)->key < key) {
                 newNode = &((*newNode)->right);
-            else {
+            } else {
                 newNode = nullptr;
                 break;
             }
@@ -122,16 +137,46 @@ class AVLDict {
         if (newNode) {
             (*newNode).reset(new node(key, value, parent, this));
             size++;
-            auto current = *newNode;
-            while (current) {
-                if (current->getBalanceFactor() > 1 || current->getBalanceFactor() < -1) {
-                    current->balance();
-                } else {
-                    current->setHeight();
-                }
-                current = current->parent.lock();
+            fixFrom(*newNode);
+        }
+    }
+    void deleteByKey(T key) {
+        auto nodeToDelete = this->head;
+        while (nodeToDelete && nodeToDelete->key != key) {
+            if (nodeToDelete->key > key) {
+                nodeToDelete = nodeToDelete->left;
+            } else {
+                nodeToDelete = nodeToDelete->right;
             }
         }
+        auto fix = nodeToDelete->parent.lock();
+        if (!(nodeToDelete->left) && !(nodeToDelete->right)) {
+            nodeToDelete->removeFromParent(nullptr);
+        } else if (!(nodeToDelete->left)) {
+            nodeToDelete->removeFromParent(nodeToDelete->right);
+        } else if (!(nodeToDelete->right)) {
+            nodeToDelete->removeFromParent(nodeToDelete->left);
+        } else if (nodeToDelete->left && nodeToDelete->right) {
+            auto swapWith = nodeToDelete->right;
+            while (swapWith->left) {
+                swapWith = swapWith->left;
+            }
+            if (swapWith != nodeToDelete->right) {
+                auto sr = swapWith->right;
+                auto sp = swapWith->parent.lock();
+                sp->left = sr;
+                if (sr) {
+                    sr->parent = sp;
+                }
+                swapWith->right = nodeToDelete->right;
+                nodeToDelete->right->parent = swapWith;
+                fix = swapWith->parent.lock();
+            }
+            swapWith->left = nodeToDelete->left;
+            nodeToDelete->left->parent = swapWith;
+            nodeToDelete->removeFromParent(swapWith);
+        }
+        fixFrom(fix);
     }
     friend std::ostream& operator<<(std::ostream& os, const AVLDict& avl) {
         return os << avl.head << endl;
@@ -139,13 +184,19 @@ class AVLDict {
     int getHeight() {
         return this->head->getHeight();
     }
+    void printByOrder() {
+        printByOrder(this->head);
+    }
 };
 int main() {
     AVLDict<int, int> t;
+    t.insert(77, 77);
     srand(0);
-    for (int i = 0; i < 1000000; i++) {
-        int x = rand();
-        t.insert(i, i);
-        cout << t.getHeight() << "," << i << "," << t.size << endl;
+    for (int i = 0; i < 100; i++) {
+        int x = rand() % 500;
+        t.insert(x, x);
+        // cout << t.getHeight() << "," << i << "," << t.size << endl;
     }
+    t.deleteByKey(77);
+    t.printByOrder();
 }
